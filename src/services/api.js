796b01ef58ecdb09789abfa78632984417a45ca2
doasis77 <amegahbill@gmail.com>
2024-12-16 12:54:03 +0000

    Moditifications to api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
+const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
 
-const baseURL = import.meta.env.BACKEND_URL || 'http://localhost:5000/api'
+
+// Create an Axios instance
 const api = axios.create({
-    baseURL // Replace with your backend base URL
+  baseURL,
 });
+
 
 export default api;
